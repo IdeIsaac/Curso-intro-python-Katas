99c2a415643a39f2d702ceb9b6430d7fecf1e20b
Isaac Hernandez <96219383+IdeIsaac@users.noreply.github.com>
2022-02-11 14:27:22 -0600

    Kata nnúmero 6

---
+++ Kata-2-Isaac-Hernandez.py
@@ -0,0 +1,9 @@
+#Creacion de un entorno virtual se sguieron los pasos
+#acontinuacion
+print('Hola, mundo')
+#python -m venv env
+#env\Scripts\activate
+#pip freeze
+#pip install python-dateutil
+#pip freeze
+#deactivate
